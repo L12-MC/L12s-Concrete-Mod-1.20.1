7220c4ca2451a6114140d70bb041116696c80c6c
Labros Karatasios <lampros.karatasios@gmail.com>
2025-05-20 15:16:40 +0300

    mod.json changes more

---
+++ src/main/java/net/l12/l12concrete/L12sConcreteModClient.java
@@ -0,0 +1,10 @@
+package net.l12.l12concrete;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class L12sConcreteModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
