3995eb43c8977bd55549eda36d94d3d7658ec9be
Labros Karatasios <lampros.karatasios@gmail.com>
2025-05-20 14:55:12 +0300

    Initial commit

---
+++ src/main/java/net/l12/l12concrete/L12sConcreteModDataGenerator.java
@@ -0,0 +1,11 @@
+package net.l12.l12concrete;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class L12sConcreteModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
